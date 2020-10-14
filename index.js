6d0c14779384c6faa462de6b18247a0d5971d451
Ashish Ranjan <rnshis@amazon.com>
2020-10-14 14:53:34 +0530

    Adds Store to the application.
    Signed-off-by: Ashish Ranjan <rnshis@amazon.com>

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
 const BUY_CAKE = "BUY_CAKE";
 
 function buyCake() {
@@ -24,3 +27,11 @@ const reducer = (state = initialState, action) => {
             return state
     }
 }
+
+const store = createStore(reducer);
+console.log('initial state', store.getState());
+const unsubscribe = store.subscribe(() => console.log("updated state", store.getState()));
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+unsubscribe();
