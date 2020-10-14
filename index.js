989617211d56bcdc2ada69191f29e6eef004fc3f
Ashish Ranjan <rnshis@amazon.com>
2020-10-14 15:02:00 +0530

    Adds buy ice cream functionality to the app.
    Signed-off-by: Ashish Ranjan <rnshis@amazon.com>

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const redux = require('redux');
 const createStore = redux.createStore;
 
 const BUY_CAKE = "BUY_CAKE";
+const BUY_ICE_CREAMS = "BUY_ICE_CREAMS";
 
 function buyCake() {
     return {
@@ -10,10 +11,17 @@ function buyCake() {
     };
 }
 
-// (previousState, action) => newState
+function buyIceCreams() {
+    return {
+        type: BUY_ICE_CREAMS,
+        info: "First redux action"
+    };
+}
 
+// (previousState, action) => newState
 const initialState = {
-    numOfCakes: 10
+    numOfCakes: 10,
+    numOfIceCreams: 20
 }
 
 const reducer = (state = initialState, action) => {
@@ -22,6 +30,11 @@ const reducer = (state = initialState, action) => {
             return {
                 ...state, // First create a copy before modifying
                 numOfCakes: state.numOfCakes - 1
+            }
+        case BUY_ICE_CREAMS:
+            return {
+                ...state,
+                numOfIceCreams: state.numOfIceCreams -1
             }
         default:
             return state
@@ -34,4 +47,6 @@ const unsubscribe = store.subscribe(() => console.log("updated state", store.get
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
+store.dispatch(buyIceCreams());
+store.dispatch(buyIceCreams());
 unsubscribe();
