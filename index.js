b8ea583c9a44d55160a4537122a8005ca0b83263
Ashish Ranjan <rnshis@amazon.com>
2020-10-23 21:46:35 +0530

    Adds redux logger as middleware
    This commit adds redux logger as middleware.
    
    Signed-off-by: Ashish Ranjan <rnshis@amazon.com>

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const redux = require('redux');
+const reduxLogger = require('redux-logger');
 const createStore = redux.createStore;
 const combinedReducers = redux.combineReducers;
+const applyMiddleWare = redux.applyMiddleware;
 
+const logger = reduxLogger.createLogger();
 const BUY_CAKE = "BUY_CAKE";
 const BUY_ICE_CREAMS = "BUY_ICE_CREAMS";
 
@@ -58,9 +61,9 @@ const rootReducer = combinedReducers({
     iceCream: iceCreamReducer,
 })
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer,applyMiddleWare(logger));
 console.log('initial state', store.getState());
-const unsubscribe = store.subscribe(() => console.log("updated state", store.getState()));
+const unsubscribe = store.subscribe(() => {});
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
