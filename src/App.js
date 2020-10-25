fba22ca475e161cadb55de738770f83e07db7a69
Ashish Ranjan <rnshis@amazon.com>
2020-10-25 18:08:59 +0530

    Adds hooks container for ice cream
    Signed-off-by: Ashish Ranjan <rnshis@amazon.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import CakeContainer from './components/CakeContainer';
+import IceCreamContainer from "./components/IceCreamContainer";
 import HooksCakeContainer from './components/HooksCakeContainer';
+import HooksIceCreamContainer from "./components/HooksIceCreamContainer";
 import {Provider} from 'react-redux'
 import store from './redux/store'
 import React from "react";
@@ -10,7 +12,9 @@ function App() {
         <Provider store={store}>
             <div className="App">
                 <HooksCakeContainer/>
+                <HooksIceCreamContainer/>
                 <CakeContainer/>
+                <IceCreamContainer/>
             </div>
         </Provider>
     );
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,1 +1,2 @@
-export {buyCake} from './cakes/cakeActions'
+export {buyCake} from './cake/cakeActions'
+export {buyIceCream} from './iceCream/iceCreamActions'
