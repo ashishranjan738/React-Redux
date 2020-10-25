36b59163d6e81d6edee1e475df4842c53f14c1e3
Ashish Ranjan <rnshis@amazon.com>
2020-10-25 17:35:44 +0530

    Implement cakeContainers with hooks
    Signed-off-by: Ashish Ranjan <rnshis@amazon.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import CakeContainer from './components/CakeContainer';
+import HooksCakeContainer from './components/HooksCakeContainer';
 import {Provider} from 'react-redux'
 import store from './redux/store'
+import React from "react";
 
 function App() {
     return (
         <Provider store={store}>
             <div className="App">
+                <HooksCakeContainer/>
                 <CakeContainer/>
             </div>
         </Provider>
